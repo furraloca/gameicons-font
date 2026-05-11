b6bf3bc78ca0bbea4c9be963b1b5054230f804b5
NamiStuff <36270268+NamiStuff@users.noreply.github.com>
2026-05-11 18:46:07 +0200

    Update download-and-format-icons.js

--- download-and-format-icons.js
+++ download-and-format-icons.js
@@ -7,7 +7,8 @@ const extract = require('unzipper');
 const { svg2png } = require('svg-png-converter');
 const webfontsGenerator = require('webfonts-generator');
 
-const BASE_URL = 'https://game-icons.net/archives/svg/zip/000000/transparent/game-icons.net.svg.zip';
+// const BASE_URL = 'https://game-icons.net/archives/svg/zip/000000/transparent/game-icons.net.svg.zip';
+const BASE_URL = 'https://github.com/NamiStuff/gameicons-font/raw/refs/heads/master/files/game-icons.net.svg.zip';
 
 // Variables to define the font class and icon prefix
 const fontClass = 'gi',
